8841db8c605d22b73a86669d48fc68c6a894e249
Hoàng Anh Sơn <110971608+hoangson36@users.noreply.github.com>
2022-09-13 21:05:57 +0700

    Create VL16.cpp

---
+++ VL16.cpp
@@ -0,0 +1,25 @@
+using System;
+
+namespace Vl16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            var s1 = s.Split(' ');
+            int a = Convert.ToInt32(s1[0]);
+            int b = Convert.ToInt32(s1[1]);
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            for(int i=1;i<=a*b;i++)
+                if((i%a==0)&&(i%b==0))
+                {
+                    Console.WriteLine(i);
+                    break;
+                }       
+            
+        }
+    }
+}
